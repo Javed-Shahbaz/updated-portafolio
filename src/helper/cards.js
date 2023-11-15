e2979f588edfe080402514ca21d192785872f46b
Javed-Shahbaz <javedshahbaz007@gmail.com>
2023-11-16 00:00:11 +0500

    update cards

--- src/helper/cards.js
+++ src/helper/cards.js
@@ -8,19 +8,6 @@ import htmlProject from "../asset/images/projects_imgs/html-project.png";
 const projectDetailList = [
   {
     id: 0,
-    name: "Art exposition Event",
-    description:
-      "This project aims  to re-create a professional website for an event. ",
-    imageLink: htmlProject,
-    technologies: ["HTML", "CSS", "JavaScript"],
-    role: "FrontEnd Dev",
-    company: "Microverse",
-    year: "2023",
-    repoLink: "https://github.com/Javed-Shahbaz",
-    demoLink: "https://Javed-Shahbaz.github.io/Capstone-Project/index.html",
-  },
-  {
-    id: 1,
     name: "Popcorn Pulse",
     description:
       "Popcorn Pulse is a web app that uses Webpack, Jest, HTML, SCSS, and JavaScript. It fetches data from a movie API and displays the latest trendy movies. Users can like or leave comments on movies, with like and comment counts stored using another API.",
@@ -33,7 +20,7 @@ const projectDetailList = [
     demoLink: "https://wonderful-tulumba-37d181.netlify.app/",
   },
   {
-    id: 2,
+    id: 1,
     name: "Space X",
     description:
       "This project is a web application that focus on rocket reservations and mission management. It allows users to reserve rockets for upcoming missions and provides a comprehensive overview of mission details. The application is built using React.js and Redux for state management.",
@@ -46,7 +33,7 @@ const projectDetailList = [
     demoLink: "https://grand-platypus-2cdfa7.netlify.app/",
   },
   {
-    id: 3,
+    id: 2,
     name: "Math Magician",
     description:
       "Math Magicians Calculator app is a website for fans of mathematics. It is a Single Page App (SPA) that allows users to: 1)Make simple calculations. 2)Read a random math-related quote by fetching from an Api.",
