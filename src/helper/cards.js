470ed891f6ebf54cb2e57a5adfec3216e93f8d72
Javed-Shahbaz <javedshahbaz007@gmail.com>
2023-11-15 23:04:37 +0500

    update files

--- src/helper/cards.js
+++ src/helper/cards.js
@@ -1,59 +1,63 @@
 // import bookstorereact3 from '../asset/images/projects_imgs/bookstorereact3.png';
-import movies2 from '../asset/images/projects_imgs/movies2.png';
-import spaceX from '../asset/images/projects_imgs/spaceX.png';
+import movies2 from "../asset/images/projects_imgs/movies2.png";
+import spaceX from "../asset/images/projects_imgs/spaceX.png";
 // import stock from '../asset/images/projects_imgs/stock.png';
-import math from '../asset/images/projects_imgs/math-sc.png';
-import htmlProject from '../asset/images/projects_imgs/html-project.png';
+import math from "../asset/images/projects_imgs/math-sc.png";
+import htmlProject from "../asset/images/projects_imgs/html-project.png";
 
 const projectDetailList = [
-    {
-      id: 0,
-      name: 'Art exposition Event',
-      description: 'this project aims  to recreate a professional website for an event. ',
-      imageLink: htmlProject,
-      technologies: ['html', 'css', 'javascript'],
-      role: 'FrontEnd Dev',
-      company: 'Microverse',
-      year: '2023',
-      repoLink: 'https://github.com/danielamoreno699/Capstone-Project',
-      demoLink: 'https://danielamoreno699.github.io/Capstone-Project/index.html',
-    },
-    {
-      id: 1,
-      name: 'Popcorn Pulse',
-      description: 'PopcornPulse is a web app that uses Webpack, Jest, HTML, SCSS, and JavaScript. It fetches data from a movie API and displays the latest trendy movies. Users can like or leave comments on movies, with like and comment counts stored using another API',
-      imageLink: movies2,
-      technologies: ['html', 'css', 'javascript', 'Bootstrap'],
-      role: 'FrontEnd Dev',
-      company: 'Microverse',
-      year: '2023',
-      repoLink: 'https://github.com/danielamoreno699/Capstone-projectJs',
-      demoLink: 'https://wonderful-tulumba-37d181.netlify.app/',
-    },
-    {
-      id: 2,
-      name: 'Space X',
-      description: 'This project is a web application that focuses on rocket reservations and mission management. It allows users to reserve rockets for upcoming missions and provides a comprehensive overview of mission details. The application is built using React.js and Redux for state management ',
-      imageLink: spaceX,
-      technologies: ['html', 'css', 'React', 'Redux'],
-      role: 'FrontEnd Dev',
-      company: 'Microverse',
-      year: '2023',
-      repoLink: 'https://github.com/danielamoreno699/space-x-project',
-      demoLink: 'https://grand-platypus-2cdfa7.netlify.app/',
-    },
-    {
-      id: 3,
-      name: 'Math Magician',
-      description: 'Math Magicians Calculator app is a website for all fans of mathematics. It is a Single Page App (SPA) that allows users to: 1)Make simple calculations. 2)Read a random math-related quote by fetching from an Api.',
-      imageLink: math,
-      technologies: ['html', 'css', 'React', 'Redux'],
-      role: 'FrontEnd Dev',
-      company: 'Microverse',
-      year: '2023',
-      repoLink: 'https://github.com/danielamoreno699/Math-Magician',
-      demoLink: 'https://math-magician-dvak.onrender.com',
-    },
-  ];
+  {
+    id: 0,
+    name: "Art exposition Event",
+    description:
+      "This project aims  to re-create a professional website for an event. ",
+    imageLink: htmlProject,
+    technologies: ["HTML", "CSS", "JavaScript"],
+    role: "FrontEnd Dev",
+    company: "Microverse",
+    year: "2023",
+    repoLink: "https://github.com/Javed-Shahbaz",
+    demoLink: "https://Javed-Shahbaz.github.io/Capstone-Project/index.html",
+  },
+  {
+    id: 1,
+    name: "Popcorn Pulse",
+    description:
+      "Popcorn Pulse is a web app that uses Webpack, Jest, HTML, SCSS, and JavaScript. It fetches data from a movie API and displays the latest trendy movies. Users can like or leave comments on movies, with like and comment counts stored using another API.",
+    imageLink: movies2,
+    technologies: ["HTML", "CSS", "JavaScript", "Bootstrap"],
+    role: "FrontEnd Dev",
+    company: "Microverse",
+    year: "2023",
+    repoLink: "https://github.com/Javed-Shahbaz",
+    demoLink: "https://wonderful-tulumba-37d181.netlify.app/",
+  },
+  {
+    id: 2,
+    name: "Space X",
+    description:
+      "This project is a web application that focus on rocket reservations and mission management. It allows users to reserve rockets for upcoming missions and provides a comprehensive overview of mission details. The application is built using React.js and Redux for state management.",
+    imageLink: spaceX,
+    technologies: ["HTML", "CSS", "React", "Redux"],
+    role: "FrontEnd Dev",
+    company: "Microverse",
+    year: "2023",
+    repoLink: "https://github.com/Javed-Shahbaz",
+    demoLink: "https://grand-platypus-2cdfa7.netlify.app/",
+  },
+  {
+    id: 3,
+    name: "Math Magician",
+    description:
+      "Math Magicians Calculator app is a website for fans of mathematics. It is a Single Page App (SPA) that allows users to: 1)Make simple calculations. 2)Read a random math-related quote by fetching from an Api.",
+    imageLink: math,
+    technologies: ["HTML", "CSS", "React", "Redux"],
+    role: "FrontEnd Dev",
+    company: "Microverse",
+    year: "2023",
+    repoLink: "https://github.com/Javed-Shahbaz",
+    demoLink: "https://math-magician-dvak.onrender.com",
+  },
+];
 
-  export default projectDetailList
+export default projectDetailList;
--- src/helper/topproj.js
+++ src/helper/topproj.js
@@ -1,40 +1,39 @@
-import wanderpal from '../asset/images/projects_imgs/wanderpal-resize.png';
+import wanderpal from "../asset/images/projects_imgs/wanderpal-resize.png";
 
 // import wandertest from '../asset/images/projects_imgs/wandertest.png';
-import wander1 from '../asset/images/projects_imgs/wanderpal/wander1.png';
-import wander2 from '../asset/images/projects_imgs/wanderpal/wander2.png'
-import wander3 from '../asset/images/projects_imgs/wanderpal/wander3.png'
-import wander4 from '../asset/images/projects_imgs/wanderpal/wander4.png'
+import wander1 from "../asset/images/projects_imgs/wanderpal/wander1.png";
+import wander2 from "../asset/images/projects_imgs/wanderpal/wander2.png";
+import wander3 from "../asset/images/projects_imgs/wanderpal/wander3.png";
+import wander4 from "../asset/images/projects_imgs/wanderpal/wander4.png";
 // import costtest from '../asset/images/projects_imgs/costtest.png';
-import cutcost_resize from '../asset/images/projects_imgs/cost-resize.png';
-import cost1 from '../asset/images/projects_imgs/costapp/costcut1.png';
-import cost2 from '../asset/images/projects_imgs/costapp/costcut2.png';
-import cost3 from '../asset/images/projects_imgs/costapp/costcut3.png';
-import cost4 from '../asset/images/projects_imgs/costapp/costcut4.png';
+import cutcost_resize from "../asset/images/projects_imgs/cost-resize.png";
+import cost1 from "../asset/images/projects_imgs/costapp/costcut1.png";
+import cost2 from "../asset/images/projects_imgs/costapp/costcut2.png";
+import cost3 from "../asset/images/projects_imgs/costapp/costcut3.png";
+import cost4 from "../asset/images/projects_imgs/costapp/costcut4.png";
 
-import stockProject from '../asset/images/projects_imgs/test2.png';
-import stock1 from '../asset/images/projects_imgs/stock-pr/stock-project1.png';
-import stock2 from '../asset/images/projects_imgs/stock-pr/stock-project2.png';
-import stock3 from '../asset/images/projects_imgs/stock-pr/stock-project3.png';
-import stock4 from '../asset/images/projects_imgs/stock-pr/stock-project4.png';
+import stockProject from "../asset/images/projects_imgs/test2.png";
+import stock1 from "../asset/images/projects_imgs/stock-pr/stock-project1.png";
+import stock2 from "../asset/images/projects_imgs/stock-pr/stock-project2.png";
+import stock3 from "../asset/images/projects_imgs/stock-pr/stock-project3.png";
+import stock4 from "../asset/images/projects_imgs/stock-pr/stock-project4.png";
 
 const projectsTop = [
   {
     id: 1,
-    name: 'Wanderpal Fullstack',
-    description: `The app is built  with React Vite in the Front-End and Rails in the Back-end.`,
+    name: "Wanderpal Full Stack",
+    description: `The app is built  with React-Vite in the Front-End and Rails in the Back-end.`,
 
-
-    fullDescription: `The app is built  with React Vite in the Front-End and Rails in the Back-end.
+    fullDescription: `The app is built  with React-Vite in the Front-End and Rails in the Back-End.
 
     The primary functionality of the app is to allow users to book reservations for tours. To access this functionality, users need to be logged in.
     
     The app supports different types of requests that users can execute. These requests include creating, retrieving (getting), and deleting reservations and items. This indicates that the app has a comprehensive set of features related to managing reservations and items associated with tours.`,
     image: wanderpal,
-    year: '2023',
-    technologies: ['react', 'redux', 'bootstrap', 'rails', 'ruby', 'psql'],
-    repoLink: 'https://github.com/danielamoreno699/wanderpal-frontend',
-    demoLink: 'https://fancy-toffee-1b590d.netlify.app',
+    year: "2023",
+    technologies: ["React", "Redux", "Bootstrap", "Rails", "Ruby", "PSQL"],
+    repoLink: "https://github.com/Javed-Shahbaz",
+    demoLink: "https://fancy-toffee-1b590d.netlify.app",
     imageProjectItem1: wander1,
     imageProjectItem2: wander2,
     imageProjectItem3: wander3,
@@ -42,15 +41,15 @@ const projectsTop = [
   },
   {
     id: 2,
-    name: 'Cost-Cut App',
+    name: "Cost-Cut App",
     description: `A rails built app for budget managing.`,
     fullDescription: `The Ruby on Rails Cost Cut App application exemplifies the MVC architecture and leverages the Devise gem for authentication and the CanCanCan gem for authorization. Demonstrating the robust capabilities of Ruby on Rails, this app seamlessly interacts with a database to deliver an impressive user experience.
-    This system provides users with enhanced capabilities for managing their budgets. Users are empowered to efficiently handle their finances through various features`,
+    This system provides users with enhanced capabilities for managing their budgets. Users are empowered to efficiently handle their finances through various features.`,
     image: cutcost_resize,
-    year: '2023',
-    technologies: [ 'rails', 'ruby', 'psql'],
-    repoLink: 'https://github.com/danielamoreno699/CostCutApp',
-    demoLink: 'https://rails-emv5.onrender.com',
+    year: "2023",
+    technologies: ["Rails", "Ruby", "PSQL"],
+    repoLink: "https://github.com/Javed-Shahbaz",
+    demoLink: "https://rails-emv5.onrender.com",
     imageProjectItem1: cost1,
     imageProjectItem2: cost2,
     imageProjectItem3: cost3,
@@ -58,24 +57,23 @@ const projectsTop = [
   },
   {
     id: 3,
-    name: 'Stock App',
+    name: "Stock App",
     description: `This project is a React-Redux app that allows users to search for stocks .`,
     fullDescription: `This project is a web and mobile application that takes inspiration from the design of Nelson Sawka. 
     It consists of two pages: the home page and the detail page. 
-    The home page showcases a selection of stocks from the Stock Market Exchange, each represented by its symbol (e.g., AAPL for APPLE Inc).
+    The home page showcases a selection of stocks from the Stock Market Exchange, each represented by its symbol (e.g., AAPLE for APPLE Inc.).
      Additionally, it displays the company's CIK as a numerical value, which serves as the registration number of the company before the SEC. 
-     The data used in the application is obtained from the Polygon API documentation`,
+     The data used in the application is obtained from the Polygon API documentation.`,
     image: stockProject,
-    year: '2023',
-    technologies: [ 'react', 'redux', 'javascript'],
-    repoLink: 'https://github.com/danielamoreno699/react-project-capstone',
-    demoLink: 'https://venerable-sprinkles-b40b30.netlify.app',
+    year: "2023",
+    technologies: ["React", "Redux", "JavaScript"],
+    repoLink: "https://github.com/Javed-Shahbaz",
+    demoLink: "https://venerable-sprinkles-b40b30.netlify.app",
     imageProjectItem1: stock1,
     imageProjectItem2: stock2,
     imageProjectItem3: stock3,
     imageProjectItem4: stock4,
   },
-
 ];
 
 export default projectsTop;
